c7e57c21d09e15ac3e5c8850ebd7d10219a87bbc
colus3 <colus4@gmail.com>
2019-12-11 23:51:38 +0900

    ch5 수정중...

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const expressSession = require('express-session');
+const dotenv = require('dotenv');
+const passport = require('passport');
 
+const passportConfig = require('./passport');
+const db = require('./models');
+const userAPIRouter = require('./routes/user');
+const postrAPIRouter = require('./routes/post');
+const postsrAPIRouter = require('./routes/posts');
+
+dotenv.config();
 const app = express();
+db.sequelize.sync();
+passportConfig();
 
-app.get('/', (req, res) => {
-  res.send('Hello server');
-});
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(expressSession({
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false, // https를 쓸 때 true
+  },
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get('/about', (req, res) => {
-  res.send('Hello, about');
-});
+// API는 다른 서비스가 내 서비스의 기능을 실행할 수 있게 열어둔 창구
+app.use('/api/user', userAPIRouter);
+app.use('/api/post', postrAPIRouter);
+app.use('/api/posts', postsrAPIRouter);
 
 app.listen(3065, () => {
   console.log('server is running on http://localhost:3065');
--- models/index.js
+++ models/index.js
@@ -1,7 +1,6 @@
 const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '../config/config.json')[env];
+const config = require('../config/config.js')[env];
 const db = {};
 
 let sequelize = new Sequelize(config.database, config.username, config.password, config);
@@ -11,6 +10,12 @@ Object.keys(db).forEach(modelName => {
     db[modelName].associate(db);
   }
 });
+
+db.Comment = require('./comment')(sequelize, Sequelize);
+db.Hashtag = require('./hashtag')(sequelize, Sequelize);
+db.Image = require('./image')(sequelize, Sequelize);
+db.Post = require('./post')(sequelize, Sequelize);
+db.User = require('./user')(sequelize, Sequelize);
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
